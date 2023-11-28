f779a565b110596c3b8512529525f5694906a34c
shrook <shrookalattar8@gmail.com>
2023-11-28 06:05:22 -0800

    a

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
 for i in range(97, 123):
-    number = 0 if (chr(i) == 'q' or chr(i) == 'e') else i
+    number = 0
+    if (chr(i) == 'q' or chr(i) == 'e')
+    else i
     print('{}'.format(chr(number)), end='')
