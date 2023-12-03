fe6e37957f96175fea5b458b9a839a71156b4e3e
shrook <shrookalattar8@gmail.com>
2023-12-03 10:51:53 -0800

    a

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if not matrix:
-        return None
-    for submatrix in matrix:
-        if len(submatrix) == 0:
-            print()
-        for i in range(len(submatrix)):
-            print("{:d}".format(submatrix[i]),
-                    end="\n" if i is len(submatrix) - 1 else " ")
+    """Function that prints a matrix of integers"""
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            if j != 0:
+                print(" ", end='')
+            print("{:d}".format(matrix[i][j]), end='')
+        print()
